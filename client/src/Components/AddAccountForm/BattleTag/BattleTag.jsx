908639ccd9221de9d5d09dcab3996b9b3861bf31
Dave Funk <davefunk135@gmail.com>
2018-06-25 03:19:27 -0700

    working on clientside form Validation

--- client/src/Components/AddAccountForm/BattleTag/BattleTag.jsx
+++ client/src/Components/AddAccountForm/BattleTag/BattleTag.jsx
@@ -2,38 +2,64 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as modalActionCreators from '../../../actions/modalActions';
-import * as accountsSavedFormActionCreators from '../../../actions/accountsSavedActions';   
+import * as accountsSavedFormActionCreators from '../../../actions/accountsSavedActions';
 class BattleTag extends Component {
     state = {
         BattleTag: "",
-      };
-    
-      handleInputChange = event => {
-          let value = event.target.value;
-          let name = event.target.name;
-          this.setState({
-              [name]: value
-          });
-          console.log(`BattleTag input: ${this.state.BattleTag}`)
-      };
+        Errors: [],
+    };
 
-      handleFormSubmit = event => {
-          event.preventDefault();
-          let BattleTag = this.state.BattleTag;
-          this.props.accountsSavedFormActions.saveBattleTag(BattleTag)
-          this.props.accountsSavedFormActions.signupStep()
-          console.log(this.state.BattleTag)
-      }
+    validate = (BattleTag) => {
+
+        if (BattleTag.length === 0 && !this.state.Errors.includes('Battletag Cannot be Empty Field')) {
+            console.log('doesnt include')
+            this.setState({ Errors: this.state.Errors.concat('Battletag Cannot be Empty Field') });
+            
+            return this.state.Errors;
+          
+        }
+        console.log(this.state.Errors)
+        if(BattleTag.length === 0 && this.state.Errors.includes('Battletag Cannot be Empty Field')) {
+            console.log('includes!')
+            return this.state.Errors;
+        }
+        if (!BattleTag.length === 0) {
+            this.setState({Errors: []})
+            this.props.accountsSavedFormActions.saveBattleTag(BattleTag)
+            this.props.accountsSavedFormActions.signupStep()            
+        }
+        console.log(this.state.Errors)
+    }
+
+    handleInputChange = event => {
+        let value = event.target.value;
+        let name = event.target.name;
+        this.setState({
+            [name]: value
+        });
+        console.log(`BattleTag input: ${this.state.BattleTag}`)
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        let BattleTag = this.state.BattleTag;
+        this.validate(BattleTag)
+    }
 
 
     render() {
+
         return (
             <div className='account-form'>
-                    <h1 className='u-margin-bottom-small'>please tell us your battletag.</h1>
+                <h1 className='u-margin-bottom-small'>please tell us your battletag.</h1>
                 <p>No account numbers are required, this is simply to put at the top of your spreadsheet in case you want to track another account.</p>
                 <form className="account-form__form">
+                {this.state.Errors.length ? <div>{this.state.Errors.map(error => {
+                    <p>{error.error}</p>
+                })}</div>:<div></div>  }
                     <input
-                    className='account-form__form--BattleTag'
+                    
+                        className='account-form__form--BattleTag'
                         value={this.state.BattleTag}
                         name="BattleTag"
                         onChange={this.handleInputChange}
@@ -46,6 +72,7 @@ class BattleTag extends Component {
         )
     }
 }
+
 function mapStateToProps(state) {
     return {
         showModal: state.showModal,
